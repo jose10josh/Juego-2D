2755ba561e16fe58a527a632be026b55c580d4d8
José Arévalo <joseaf1002@gmail.com>
2022-05-30 19:04:55 -0600

    Add dash movement

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -6,34 +6,23 @@ public class PlayerController : MonoBehaviour
     [Header("Components")]
     private Rigidbody2D _rigidBody;
     private Animator _animator;
-    //private CapsuleCollider2D _boxCollider;
 
     [Header("Player Statistics")]
-    [SerializeField]
     private float movementVelocity = 10;
-    [SerializeField]
-    private float jumpForce = 7;
+    private float jumpForce = 8;
+    float rollForce = 1.5f;
 
     [Header("Conditionals")]
     private bool canMove = true;
-    [SerializeField]
     private int jumpCount = 0;
-    private readonly int jumpLimit = 2;
+    private readonly int jumpLimit = 1;
     public bool isOnGround = true;
-
-    [Header("Collisions")]
-    [SerializeField]
-    private Vector2 playerFeet = new(0, -0.65f);
-    private readonly float radioCollider = 0.2f;
-    [SerializeField]
-    private LayerMask floorLayer;
-
+    public bool isRolling = false;
 
     private void Awake()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-        //_boxCollider = GetComponent<CapsuleCollider2D>();
     }
 
     private void Update()
@@ -42,46 +31,68 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector2 direction = new(horizontalInput, verticalInput);
+
         Walk(direction);
+
+        StartRolling(direction);
 
         FixJump();
         if (Input.GetKeyDown(KeyCode.Space) && jumpCount < jumpLimit)
         {
             _animator.SetBool("Jump", true);
+            StopRoll();
             jumpCount++;
             Jump();
         }
-            
+
+       
+    }
+
+    private void StartRolling(Vector2 direction)
+    {
+        if (Input.GetKeyDown(KeyCode.X) && !isRolling)
+        {
+            isRolling = true;
+            _animator.SetBool("Roll", true);
+            _animator.SetBool("Jump", false);
+            Camera.main.GetComponent<RippleEffect>().Emit(Camera.main.WorldToViewportPoint(transform.position));
+        }
+        if(isRolling)
+        {
+            float force = rollForce;
+            if (transform.eulerAngles.y == 180)
+                force = -rollForce;
+
+            MovePlayer(new Vector2(force * movementVelocity, _rigidBody.velocity.y));
+        }
+    }
+
+    private void StopRoll()
+    {
+        isRolling = false;
+        _rigidBody.velocity = new Vector2(0, _rigidBody.velocity.y);
+        _animator.SetBool("Roll", false);
     }
 
     public void LandOnGround()
     {
-        _animator.SetBool("Fall", false);
+        _animator.SetBool("Jump", false);
         jumpCount = 0;
-    }
-
-    /// <summary>
-    /// Validate player is on ground using a Box cast
-    /// </summary>
-    private void IsOnGround()
-    {
-        //float extraHeight = 0.05f;
-        ////RaycastHit2D raycastHit = Physics2D.Raycast(_boxCollider.bounds.center, Vector2.down, _boxCollider.bounds.extents.y + extraHeight, floorLayer);
-        //RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, Vector2.down, extraHeight, floorLayer);
-
-        //Color rayColor = Color.red;
-        //if (raycastHit.collider != null)
-        //    rayColor = Color.blue;
-
-        //Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), rayColor);
-        //Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), rayColor);
-        //Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y + extraHeight), Vector2.right * (_boxCollider.bounds.size.x), rayColor);
-        //isOnGround = raycastHit.collider != null;
     }
 
     private void FixJump()
     {
-        if(_rigidBody.velocity.y < 0)
+        if (!isOnGround)
+        {
+            float velocity = -1;
+            if (_rigidBody.velocity.y > 0)
+                velocity = 1;
+
+            _animator.SetFloat("VerticalVelocity", velocity);
+        }
+
+
+        if (_rigidBody.velocity.y < 0)
         {
             _rigidBody.velocity += (2.5f - 1) * Physics2D.gravity.y * Time.deltaTime * Vector2.up;
         }
@@ -92,8 +103,8 @@ public class PlayerController : MonoBehaviour
     }
     private void Jump()
     {
-        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
-        _rigidBody.velocity += Vector2.up * jumpForce;
+        //_rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
+        _rigidBody.velocity = Vector2.up * jumpForce;
     }
 
     /// <summary>
@@ -102,9 +113,9 @@ public class PlayerController : MonoBehaviour
     /// <param name="direction"></param>
     private void Walk(Vector2 direction)
     {
-        if(canMove)
+        if(canMove && !isRolling)
         {
-            _rigidBody.velocity = new Vector2(direction.x * movementVelocity, _rigidBody.velocity.y);
+            MovePlayer(new Vector2(direction.x * movementVelocity, _rigidBody.velocity.y));
 
             if (direction.x != 0)
             {
@@ -128,20 +139,47 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void VerifyIsInGround ()
+    private void MovePlayer(Vector2 direction)
     {
-        bool inGround = Physics2D.OverlapCircle((Vector2)transform.position + playerFeet, radioCollider, floorLayer);
-        Debug.Log($"Is en ground: {inGround}");
-        if (inGround)
-        {
-            jumpCount = 1;
-            _animator.SetBool("Fall", true);
-        }
+        _rigidBody.velocity = new Vector2(direction.x, direction.y);
     }
 
-    public void FinishJumping()
-    {
-        _animator.SetBool("Fall", true);
-        _animator.SetBool("Jump", false);
-    }
+    #region Old functions
+    //[Header("Collisions")]
+    //private Vector2 playerFeet = new(0, -0.65f);
+    //private readonly float radioCollider = 0.2f;
+    //[SerializeField]
+    //private LayerMask floorLayer;
+
+    //Course functions to check on ground
+    //private void VerifyIsInGround ()
+    //{
+    //    bool inGround = Physics2D.OverlapCircle((Vector2)transform.position + playerFeet, radioCollider, floorLayer);
+    //    Debug.Log($"Is en ground: {inGround}");
+    //    if (inGround)
+    //    {
+    //        jumpCount = 1;
+    //    }
+    //}
+
+    /// <summary>
+    /// Verify is on ground alternative
+    /// Validate player is on ground using a Box cast
+    /// </summary>
+    //private void IsOnGround()
+    //{
+    //float extraHeight = 0.05f;
+    ////RaycastHit2D raycastHit = Physics2D.Raycast(_boxCollider.bounds.center, Vector2.down, _boxCollider.bounds.extents.y + extraHeight, floorLayer);
+    //RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, Vector2.down, extraHeight, floorLayer);
+
+    //Color rayColor = Color.red;
+    //if (raycastHit.collider != null)
+    //    rayColor = Color.blue;
+
+    //Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), rayColor);
+    //Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), rayColor);
+    //Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y + extraHeight), Vector2.right * (_boxCollider.bounds.size.x), rayColor);
+    //isOnGround = raycastHit.collider != null;
+    //}
+    #endregion
 }
